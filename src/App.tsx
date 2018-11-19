e5094b795d93e61f4c790a674c46e45c8564a044
Vadym Holoveichuk <goloveychuk1@gmail.com>
2018-11-19 20:32:32 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,70 @@
-import React, { Component, useContext, useMemo } from "react";
+import React, {
+  Component,
+  useContext,
+  useMemo,
+  useState,
+  useEffect
+} from "react";
 import { ReactReduxContext, Provider } from "react-redux";
 import { Selector } from "reselect";
-import { combineReducers, AnyAction } from "redux";
+import { combineReducers, AnyAction,Dispatch, ActionCreator, Store } from "redux";
 
 import { createStore } from "redux";
 
+// function useSelectorOld<S>(selector: Selector<AppState, S>) {
+//   const state = useContext(ReactReduxContext).storeState as AppState;
+
+//   const val = selector(state);
+//   return val;
+// }
 
 
-
+// function useActionOld<T extends ActionCreator<any>>(action: T): T {
+//   const store = useContext(ReactReduxContext).store;
+//   return bindDispatch(action, store.dispatch)
+// }
 
 
 function useSelector<S>(selector: Selector<AppState, S>) {
-  const state = useContext(ReactReduxContext).storeState as AppState;
-  const val = selector(state);
-  return val;
+  const store = useContext(MyStoreContext)!
+
+  const [compState, setCompState] = useState(()=>selector(store.getState()));
+  let prevState = compState
+  useEffect(
+    () => {
+      console.log('calling subscribe')
+      return store.subscribe(() => {
+        const newState = selector(store.getState());
+        if (prevState !== newState) {
+          // debugger;
+          console.log('setting new state')
+          prevState = newState
+          setCompState(newState);
+        }
+      })},
+    []
+  );
+  return compState;
 }
 
-function useAction<T>(action: T): T {
-  const store = useContext(ReactReduxContext).store
-  //@ts-ignore
-  return (...args) => store.dispatch(action(...args))
+export function bindDispatch<T extends ActionCreator<any>>(
+  fn: T,
+  dispatch: Dispatch,
+): T {
+  return (((...args: any[]) => dispatch((fn as any)(...args))) as any) as T;
 }
 
+
+function useAction<T extends ActionCreator<any>>(action: T): T {
+  const store = useContext(MyStoreContext)!
+  // const [compState, setCompState] = useState(()=>selector(store.getState()));
+  return bindDispatch(action, store.dispatch)
+}
 
 ////////////////////////////////////////////////////////////////////////////////////
 
 interface CardsState {
-  list: {}[]
+  list: {}[];
 }
 
 interface CountState {
@@ -69,7 +107,10 @@ function countReducer(
   }
 }
 
-function cardsReducer(state: CardsState = {list: []}, action: any): CardsState {
+function cardsReducer(
+  state: CardsState = { list: [] },
+  action: any
+): CardsState {
   switch (action.type) {
     case "UPDATE_LIST":
       return {
@@ -86,50 +127,51 @@ const rootReducer = combineReducers({
   count: countReducer
 });
 
-
 ////////////////////////////////////////////////////////////////////////////////////////
 
-
 const Button = () => {
+  // debugger;
   const num = useSelector(countSelector);
-  const onClick = useAction(incrementAction)
-  console.log('rerendering button')
+  const onClick = useAction(incrementAction);
+  console.log("rerendering button");
   return (
     <div>
-      <button
-        onClick={onClick}
-      >{`button ${num}`}</button>
+      <button onClick={onClick}>{`button ${num}`}</button>
     </div>
   );
 };
 
 const List = () => {
   // const num = useSelector(countSelector);
-  const onClick = useAction(updateList)
-  console.log('rerendering list')
+  const onClick = useAction(updateList);
+  console.log("rerendering list");
   return (
     <div>
-      <button
-        onClick={onClick}
-      >{`update list`}
-      </button>
+      <button onClick={onClick}>{`update list`}</button>
     </div>
   );
 };
 
+function StatelessList() {
+  console.log("rerendering stateless list!!!!!!!, WHY?");
+
+  return null;
+}
+const MyStoreContext = React.createContext<Store | null>(null)
 
 class App extends Component {
   store = createStore(rootReducer);
-
   render() {
-
     return (
+      <MyStoreContext.Provider value={this.store}>
       <Provider store={this.store}>
         <div className="App">
           <Button />
           <List />
+          <StatelessList />
         </div>
       </Provider>
+      </MyStoreContext.Provider>
     );
   }
 }
